8eaaafbd9209620f8cc064381692d5eb78df7750
Yaiza16 <yaizavallejo16@gmail.com>
2021-06-25 12:50:13 +0200

    collapseMenu function done

--- dist/js/collapseMenu.js
+++ dist/js/collapseMenu.js
@@ -1,14 +1,16 @@
 export default function collapseMenu(){
     const button = document.getElementById('collapseMenu');
-    console.log(button)
+    const tracker = document.getElementById('tracker-items');
 
     button.addEventListener('click', () =>{
         if (button.classList.contains('fa-chevron-up')){
             button.classList.remove('fa-chevron-up');
             button.classList.add('fa-chevron-down')
+            tracker.classList.add('tracker-info__items--closed')
         }else if(button.classList.contains('fa-chevron-down')){
             button.classList.remove('fa-chevron-down');
             button.classList.add('fa-chevron-up')
+            tracker.classList.remove('tracker-info__items--closed')
         }
 
     })
